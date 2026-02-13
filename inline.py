5ad90c46ec9a1375b2659d5b4fb052e5f30e9d3d
gimjungwook <gomshua@gmail.com>
2026-02-13 13:33:57 +0900

    Fix mobile responsiveness for letter page

--- inline.py
+++ inline.py
@@ -1,15 +1,19 @@
 import re
+import glob
 
-# Read files
+# Read HTML
 with open(r'C:\Projects\valentine\dist\index.html', 'r', encoding='utf-8') as f:
     html = f.read()
 
-with open(r'C:\Projects\valentine\dist\assets\index-fLyFVre7.js', 'r', encoding='utf-8') as f:
-    js = f.read()
-
-# Remove external JS script and add inline script  
-html = re.sub(r'<script type="module"[^>]*\.js[^>]*></script>', '', html)
-html = html.replace('</head>', f'<script type="module">{js}</script>\n</head>')
+# Find JS file dynamically
+js_files = glob.glob(r'C:\Projects\valentine\dist\assets\index-*.js')
+if js_files:
+    with open(js_files[0], 'r', encoding='utf-8') as f:
+        js = f.read()
+    
+    # Remove external JS script and add inline script  
+    html = re.sub(r'<script type="module"[^>]*\.js[^>]*></script>', '', html)
+    html = html.replace('</head>', f'<script type="module">{js}</script>\n</head>')
 
 # Write single file
 with open(r'C:\Projects\valentine\valentine-single.html', 'w', encoding='utf-8') as f:
